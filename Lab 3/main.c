4db3ecd41fef614551a156647271748f6d82fc05
iPrescott921 <89608851+iPrescott921@users.noreply.github.com>
2024-02-20 23:50:04 -0700

    Updated the main.c to be more reader friendly.

--- Lab 3/main.c
+++ Lab 3/main.c
@@ -19,45 +19,9 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
-
-/* USER CODE BEGIN PV */
-
-/* USER CODE END PV */
-
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-/* USER CODE BEGIN PFP */
 
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
-/*
- * timer2 interrupt handler (3.1 Q5)
- */
 void TIM2_IRQHandler(void){
 		// toggle green and orange LEDs when the interrupt is triggered
 	GPIOC->ODR ^= GPIO_ODR_8;
@@ -66,29 +30,9 @@ void TIM2_IRQHandler(void){
 	// clear the pending flag in SR (3.1 Q6)
 	TIM2->SR &= ~0x00000001;
 }
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
-  /* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
-	RCC->APB1ENR |= 1;
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
-  SystemClock_Config();
-
-		// enable the GPIOC peripheral clock
+void SET_PINS(void){
+	// enable the GPIOC peripheral clock
 	RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
 
 	// set the general purpose output for the LEDs
@@ -125,6 +69,30 @@ int main(void)
 	GPIOC->AFR[0] |= (0x00 << GPIO_AFRL_AFRL6_Pos) | (0x00 << GPIO_AFRL_AFRL7_Pos);	
 	
 	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+}
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+	RCC->APB1ENR |= 1;
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+	SET_PINS();
 	
 	// configure timer2 to trigger an update event at 4Hz (default processor frequency = 8MHz)
 	TIM2->PSC = 7999;			
@@ -167,18 +135,10 @@ int main(void)
 	TIM3->CCR2 = counter;
 
 	TIM3->CR1 |= TIM_CR1_CEN_Msk;
-  /* Initialize all configured peripherals */
-	
-  /* USER CODE BEGIN 2 */
-
-  /* USER CODE END 2 */
+ 
 
   /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-  }
-  /* USER CODE END 3 */
+  while (1) {}
 }
 
 /**
@@ -216,10 +176,6 @@ void SystemClock_Config(void)
   }
 }
 
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
-
 /**
   * @brief  This function is executed in case of error occurrence.
   * @retval None
@@ -235,19 +191,3 @@ void Error_Handler(void)
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
-}
-#endif /* USE_FULL_ASSERT */
